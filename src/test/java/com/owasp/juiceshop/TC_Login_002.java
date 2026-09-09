099a62b233cde600ccd0a803033414ce58ac0872
Algohercules <adarshrreso@gmail.com>
2026-09-09 13:52:33 +0530

    TC_Login_002

---
+++ src/test/java/com/owasp/juiceshop/TC_Login_002.java
@@ -0,0 +1,45 @@
+package com.owasp.juiceshop;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC_Login_002 {
+
+    @Test
+    void validLoginTest() {
+
+        // Open Chrome
+        WebDriver driver = new ChromeDriver();
+
+        // Open OWASP Juice Shop
+        driver.get("https://juice-shop.herokuapp.com/#/login");
+
+        // Click Account
+        driver.findElement(By.xpath("//button[@id='navbarAccount']"))
+                .click();
+
+        // Click Login
+        driver.findElement(By.xpath("//button[@id='navbarLoginButton']"))
+                .click();
+
+        // Enter Email
+        driver.findElement(By.xpath("//input[@id='email']"))
+                .sendKeys("adarshrreso@gmail.com");
+
+        // Enter Password
+        driver.findElement(By.xpath("//input[@id='password']"))
+                .sendKeys("Mnbvcxz");
+
+        // Click Login button
+        driver.findElement(By.xpath("//button[@id='loginButton']"))
+                .click();
+
+        // Print current URL
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+
+        // Close browser
+        driver.quit();
+    }
+}
